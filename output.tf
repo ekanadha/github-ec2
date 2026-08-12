f67063c146c56d2677d26e5b819f89b34ef86f84
ekanadha <ekanadhareddyyatham@gmail.com>
2026-08-12 13:05:01 +0530

    update output

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "instance_id" {
-  value = aws_instance.example.id
+  value = aws_instance.apache.id
 }
 
 output "public_ip" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.apache.public_ip
 }
