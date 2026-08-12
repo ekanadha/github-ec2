28e9afd9eb27b15d72f624750fdc95cb72d83a75
ekanadha <ekanadhareddyyatham@gmail.com>
2026-08-12 09:57:01 +0530

    second commite

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
